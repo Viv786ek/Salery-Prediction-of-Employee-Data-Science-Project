63507f5d2bb4044a71ced77e821ad9f27ac03b7e
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-02-21 17:51:59 +0530

    Add files via upload

---
+++ Intercept.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn import linear_model
+import tkinter as tk 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+df=pd.read_excel('stock_price.xls')
+
+X = df[['Interest_Rate','Unemployment_Rate']].astype(float) # here we have 2 input variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
+Y = df['Stock_Index_Price'].astype(float) # output variable (what we are trying to predict)
+
+# with sklearn
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+# tkinter GUI
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 500, height = 300)
+canvas1.pack()
+
+# with sklearn
+Intercept_result = ('Intercept: ', regr.intercept_)
+label_Intercept = tk.Label(root, text=Intercept_result, justify = 'center')
+canvas1.create_window(260, 220, window=label_Intercept)
+
+# with sklearn
+Coefficients_result  = ('Coefficients: ', regr.coef_)
+label_Coefficients = tk.Label(root, text=Coefficients_result, justify = 'center')
+canvas1.create_window(260, 240, window=label_Coefficients)
+root.mainloop()
